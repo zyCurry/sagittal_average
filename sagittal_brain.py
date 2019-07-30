e0c6bd3e7c55a48a4a8bf151a901ea5cf1fdc664
Charlene Bultoc <c.bultoc@neurolab.ac.uk>
2019-07-30 17:13:04 +0100

    Having a function allows me to run it from other places

--- sagittal_brain.py
+++ sagittal_brain.py
@@ -1,23 +1,28 @@
-# Open the file to analyse
-myfile = open('brain_sample.csv', 'r')
 
-# Create a plane list to keep a list per row
-planes = []
-for line in myfile.readlines():
-    planes.append([int(x) for x in line.split("\n")[0].split(',')])
-myfile.close()
+def run_averages():
+    # Open the file to analyse
+    myfile = open('brain_sample.csv', 'r')
 
-# Create new list to save the averages per each plane
-# The number of steps coronal planes may change in the future
-coronal_planes = 20
-sagittal_averages = []
-for i in range(20):
-    total = 0
-    for j in range(coronal_planes):
-        total = total + int(planes[i][j])
-    sagittal_averages.append(str(total/coronal_planes))
+    # Create a plane list to keep a list per row
+    planes = []
+    for line in myfile.readlines():
+        planes.append([int(x) for x in line.split("\n")[0].split(',')])
+    myfile.close()
 
-# write it out on my file
-myoutput = open('brain_average.csv', 'w')
-myoutput.write(','.join(sagittal_averages) +  '\n')
-myoutput.close()
+    # Create new list to save the averages per each plane
+    # The number of steps coronal planes may change in the future
+    coronal_planes = 20
+    sagittal_averages = []
+    for i in range(20):
+        total = 0
+        for j in range(coronal_planes):
+            total = total + int(planes[i][j])
+        sagittal_averages.append(str(total/coronal_planes))
+
+    # write it out on my file
+    myoutput = open('brain_average.csv', 'w')
+    myoutput.write(','.join(sagittal_averages) +  '\n')
+    myoutput.close()
+
+if __name__ == "__main__":
+    run_averages()

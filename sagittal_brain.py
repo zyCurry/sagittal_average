7739ea22d9441436f29c6f052e100fd5bbe75c2e
Charlene Bultoc <c.bultoc@neurolab.ac.uk>
2019-09-06 20:59:42 +0100

    Adds a documentation to the function and fixes the file name parsing
    In previous versions the file name was not being parsed properly

--- sagittal_brain.py
+++ sagittal_brain.py
@@ -1,10 +1,17 @@
 import numpy as np
 
 def run_averages(file_input, file_output):
+    """
+    Calculates the average through the coronal planes
+    The input file should has as many columns as coronal planes
+    The rows are intersections of the sagittal/horizontal planes
+
+    The result is an average for each sagittal/horizontal plane (rows)
+    """
     # Open the file to analyse
     if file_input is None:
         file_input = 'brain_sample.csv'
-    with open('brain_sample.csv', 'r') as myfile:
+    with open(file_input, 'r') as myfile:
             # Create a plane list to keep a list per row
             planes = []
             for line in myfile.readlines():
@@ -27,7 +34,7 @@ def run_averages(file_input, file_output):
     # write it out on my file
     if file_output is None:
         file_output = 'brain_average.csv'
-    with open('brain_average.csv', 'w') as myoutput:
+    with open(file_output, 'w') as myoutput:
              myoutput.write(','.join(sagittal_averages) +  '\n')
 
 if __name__ == "__main__":

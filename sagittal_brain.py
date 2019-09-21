8fc86a4cb71e7c8bd6a3870a71c9c4b339451b44
Charlene Bultoc <c.bultoc@neurolab.ac.uk>
2019-09-21 13:08:30 +0100

    With Numpy I can calculate the mean without to iterate over each column

--- sagittal_brain.py
+++ sagittal_brain.py
@@ -18,16 +18,12 @@ def run_averages(file_input, file_output):
                 planes.append([int(x) for x in line.split("\n")[0].split(',')])
 
     # Create new list to save the averages per each plane
-    # The number of steps coronal planes may change in the future
-    coronal_planes = len(planes[0])
     sagittal_averages = []
     # let's use NumPy! It's faster!!
     planes = np.array(planes)
     for sagittal_sect in planes:
-        total = 0
-        for coronal_plane in sagittal_sect:
-            total = total + int(coronal_plane)
-        sagittal_averages.append(str(total/coronal_planes))
+        average = np.mean(sagittal_sect)
+        sagittal_averages.append(str(average))
 
     # write it out on my file
     if file_output is None:

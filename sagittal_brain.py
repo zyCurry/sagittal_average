95a65ee02d1f553eddece4d7a191fec0f06d5a89
Charlene Bultoc <c.bultoc@neurolab.ac.uk>
2019-09-30 14:46:42 +0100

    Adds defaults to function definition and fixes the logics with arguments
    and cleans the variables that are not used anymore

--- sagittal_brain.py
+++ sagittal_brain.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def run_averages(file_input, file_output):
+def run_averages(file_input='brain_sample.csv', file_output='brain_average.csv'):
     """
     Calculates the average through the coronal planes
     The input file should has as many columns as coronal planes
@@ -9,16 +9,12 @@ def run_averages(file_input, file_output):
     The result is an average for each sagittal/horizontal plane (rows)
     """
     # Open the file to analyse
-    if file_input is None:
-        file_input = 'brain_sample.csv'
     with open(file_input, 'r') as myfile:
             # Create a plane list to keep a list per row
             planes = []
             for line in myfile.readlines():
                 planes.append([int(x) for x in line.split("\n")[0].split(',')])
 
-    # Create new list to save the averages per each plane
-    sagittal_averages = []
     # let's use NumPy! It's faster!!
     planes = np.array(planes)
     averages = np.mean(planes, axis=0)
@@ -26,18 +22,18 @@ def run_averages(file_input, file_output):
     averages = [str(x) for x in averages]
 
     # write it out on my file
-    if file_output is None:
-        file_output = 'brain_average.csv'
     with open(file_output, 'w') as myoutput:
              myoutput.write(','.join(averages) +  '\n')
 
 if __name__ == "__main__":
     import sys
-    argumens = sys.argv
-    file_input = None
-    file_output = None
-    if len(argumens) > 1:
+    arguments = sys.argv
+    if len(arguments) == 1:
+         run_averages()
+    elif len(arguments) == 2:
          file_input = sys.argv[1]
-    if len(argumens) > 2:
+         run_averages(file_input)
+    if len(arguments) > 2:
+         file_input = sys.argv[1]
          file_output = sys.argv[2]
-    run_averages(file_input, file_output)
+         run_averages(file_input, file_output)
